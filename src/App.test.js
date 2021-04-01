421d3d346640afee106e314a74080cf0351f23ba
KaylaG1510 <14-3TaLaLeAfVeRaIeLd!mufc>
2021-04-01 21:14:22 +1300

    fixing lots of prettier syntax errors in files

--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,16 @@
 import React from "react"
-  import ReactDOM from "react-dom"
-  import App from "./App"
+import ReactDOM from "react-dom"
+import App from "./App"
 
-  describe("Addition", () => {
-    it("knows that 2 and 2 make 4", () => {
-      expect(2 +2 ).toBe(4)
-    })
+describe("Addition", () => {
+  it("knows that 2 and 2 make 4", () => {
+    expect(2 + 2).toBe(4)
   })
+})
 
-  it("renders without crashing", () => {
-    const div = document.createElement("div")
-    ReactDOM.render(<App />, div)
-    ReactDOM.unmountComponentAtNode(div)
-  })
+it("renders without crashing", () => {
+  const div = document.createElement("div")
+  ReactDOM.render(<App />, div)
+  ReactDOM.unmountComponentAtNode(div)
+})
   
