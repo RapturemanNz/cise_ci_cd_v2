ffe15ccdf45e6632af210238db7abf56d1cafbcc
KaylaG1510 <14-3TaLaLeAfVeRaIeLd!mufc>
2021-04-01 13:31:46 +1300

    fixing lots of prettier syntax errors in files

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class App extends Component {
   constructor() {
-    super();
+    super()
     this.state = {
       count: 0,
     }
   }
 
-  makeIncrementer = amount => () =>
-    this.setState(prevState =>({
-      count: prevState.count + amount,
-    }));
+  increment = this.makeIncrementer(1)
 
-    increment = this.makeIncrementer(1);
+  makeIncrementer = (amount) => () =>
+    this.setState((prevState) =>({
+      count: prevState.count + amount,
+    }))
 
     render() {
       return (
